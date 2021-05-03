3f62ba43f31979cd6367e52e7858e9e920203e7e
Muhammad Umar Nadeem <munadeem@stanford.edu>
2021-05-02 23:18:58 -0700

    small button change

--- Waves/components/EditProfileButton.js
+++ Waves/components/EditProfileButton.js
@@ -3,29 +3,29 @@ import React from 'react';
 import {StyleSheet, Text} from 'react-native';
 
 import {Button} from 'react-native-paper';
-import { DARK_GRAY, LIGHT_GRAY, MED_GRAY } from '../constants/theme';
+import {DARK_GRAY, LIGHT_GRAY, MED_GRAY} from '../constants/theme';
 
 function EditProfileButton({navigation}) {
-    return (
-        <Button
-          style={styles.button}
-          mode="outlined"
-          compact
-          onPress={() => navigation.push('Edit')}>
-            <Text style={styles.inner}>Edit Profile</Text>
-        </Button>
-    );
+  return (
+    <Button
+      style={styles.button}
+      mode="outlined"
+      uppercase={false}
+      onPress={() => navigation.push('Edit')}>
+      <Text style={styles.inner}>Edit Profile</Text>
+    </Button>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-        alignSelf: 'flex-end',
-        borderColor: MED_GRAY,
-      },
-      inner: {
-        fontSize: 13,
-        color: DARK_GRAY,
-      }
+  button: {
+    alignSelf: 'flex-end',
+    borderColor: MED_GRAY,
+  },
+  inner: {
+    fontSize: 13,
+    color: DARK_GRAY,
+  },
 });
 
 export default EditProfileButton;
