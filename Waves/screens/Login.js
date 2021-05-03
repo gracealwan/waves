14683c5282e5fd6e4d260e8a6e1870a87fc87034
Muhammad Umar Nadeem <munadeem@stanford.edu>
2021-05-02 23:14:12 -0700

    basic auth UI

--- Waves/screens/Login.js
+++ Waves/screens/Login.js
@@ -1,13 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  SafeAreaView,
-  Image,
-  ImageStore,
-} from 'react-native';
+import {StyleSheet, View, Text, SafeAreaView, Image} from 'react-native';
 import {Button} from 'react-native-paper';
 import {Input} from 'react-native-elements';
 import {PRIMARY, WHITE} from '../constants/theme';
@@ -33,24 +26,27 @@ function Login({navigation}) {
           onChangeText={value => setText(value)}
         />
         <Button
-          style={styles.button1}
+          style={styles.forgotButton}
+          uppercase={false}
           onPress={() => navigation.push('Forgot')}>
-          <Text style={styles.inner1}>Forgot Password?</Text>
+          <Text style={styles.smallButtonText}>Forgot Password?</Text>
         </Button>
       </View>
       <Button
-        style={styles.button}
-        mode="outlined"
-        compact
+        style={styles.signInButton}
+        uppercase={false}
+        mode="contained"
         onPress={() => navigation.goBack()}>
-        <Text style={styles.inner}>Sign In</Text>
+        <Text style={styles.largeButtonText}>Sign In</Text>
       </Button>
       <View style={styles.createRow}>
-        <Text style={styles.subtext}>Don't have an account?</Text>
+        <Text style={styles.bottomSubtext}>Don't have an account?</Text>
         <Button
-          style={styles.button1}
+          style={styles.createButton}
+          uppercase={false}
+          mode="text"
           onPress={() => navigation.push('Create')}>
-          <Text style={styles.inner1}>Create Account</Text>
+          <Text style={styles.smallButtonText}>Create Account</Text>
         </Button>
       </View>
     </SafeAreaView>
@@ -75,16 +71,17 @@ const styles = StyleSheet.create({
   createRow: {
     display: 'flex',
     flexDirection: 'row',
-    width: '50%',
+    width: '100%',
     justifyContent: 'center',
-    paddingTop: 50,
+    paddingTop: 20,
+    paddingBottom: 70,
   },
   title: {
     fontSize: 34,
     lineHeight: 41,
     fontWeight: 'bold',
     paddingBottom: 10,
-    paddingTop: 50,
+    paddingTop: 100,
   },
   subtitle: {
     fontSize: 17,
@@ -99,31 +96,45 @@ const styles = StyleSheet.create({
     paddingBottom: 60,
   },
   image: {
-    marginBottom: 100,
+    marginBottom: 60,
+    width: '100%',
   },
   input: {
     fontSize: 17,
     lineHeight: 22,
     color: 'rgba(60, 60, 67, 0.6)',
   },
-  button: {
-    width: '70%',
+  signInButton: {
+    width: '80%',
+    borderRadius: 14,
     backgroundColor: PRIMARY,
   },
-  button1: {
-    width: 200,
+  forgotButton: {
+    width: 190,
     backgroundColor: WHITE,
     height: 30,
+    marginTop: 5,
   },
-  inner: {
+  largeButtonText: {
     fontSize: 15,
     color: WHITE,
     fontWeight: '600',
   },
-  inner1: {
-    fontSize: 10,
+  smallButtonText: {
+    fontSize: 12,
     color: PRIMARY,
     fontWeight: '600',
+  },
+  createButton: {
+    width: 138,
+    backgroundColor: WHITE,
+    height: 30,
+    marginTop: -7,
+  },
+  bottomSubtext: {
+    fontSize: 15,
+    lineHeight: 20,
+    color: 'rgba(60, 60, 67, 0.6)',
   },
 });
 
